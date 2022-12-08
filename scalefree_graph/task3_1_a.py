6d5d6a3122635e7a6a7ca7309cdb7f4646333ad0
mnanri <nanristeps@icloud.com>
2022-12-08 19:46:25 +0900

    implement task4

--- scalefree_graph/task3_1_a.py
+++ scalefree_graph/task3_1_a.py
@@ -1,7 +1,7 @@
 from torch_geometric.nn import GCNConv
 from torch_geometric.utils.convert import from_networkx
 import torch.nn.functional as F
-import sample
+import sample2
 import torch
 import matplotlib.pyplot as plt
 
@@ -30,29 +30,31 @@ class Net(torch.nn.Module):
     return x, y
 
 def main():
-  max_nodes_per_class = 250
-  min_nodes_per_class = 40
+  max_n2_nodes_per_class = 101
+  min_n2_nodes_per_class = 20
   roop = 20
 
   guarantee_ratio_list = {}
   guarantee_number_list = {}
-  for i in range(min_nodes_per_class, max_nodes_per_class+1, 5):
-    guarantee_ratio_list[i] = []
-    guarantee_number_list[i] = []
+  for n2 in range(min_n2_nodes_per_class, max_n2_nodes_per_class+1, 3):
+    guarantee_ratio_list[n2] = []
+    guarantee_number_list[n2] = []
 
   threshold = 0.8
 
   for r in range(roop):
-    for n in range(min_nodes_per_class, max_nodes_per_class+1, 5):
+    for n2 in range(min_n2_nodes_per_class, max_n2_nodes_per_class+1, 3):
+      n1 = n2 * 4
       m = 2
       graph_num = 4
-      a,_,c = sample.generate_sample(n, m, graph_num)
+      large_graph_num = 2
+      a,_,lc,sc = sample2.generate_umblance_sample(n1, n2, m, graph_num, large_graph_num)
       dataA = from_networkx(a)
 
       device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-      model = Net(n).to(device)
+      model = Net(n1).to(device)
 
-      for cnt in range(n):
+      for cnt in range(n1):
         model.train()
         optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
@@ -61,13 +63,17 @@ def main():
 
         samples = []
         for i in range(cnt):
-          for j in c[i]:
+          for j in lc[i]:
+            samples.append(j)
+
+        for i in range(cnt//(n1//n2)):
+          for j in sc[i]:
             samples.append(j)
 
         for _ in range(epoch_num):
           optimizer.zero_grad()
-          _, out = model(dataA)
-          loss = F.nll_loss(out[samples], dataA.label[samples])
+          _, y = model(dataA)
+          loss = F.nll_loss(y[samples], t[samples])
           loss.backward()
           optimizer.step()
 
@@ -75,37 +81,38 @@ def main():
         _, out = model(dataA)
         pred = out.max(dim=1)[1]
         err = 0
-        for i, p in enumerate(pred):
+        for i,p in enumerate(pred):
           if p != t[i]:
             err += 1
-        if 1 - err / len(pred) >= threshold:
-          guarantee_ratio_list[n].append((cnt)/n)
-          guarantee_number_list[n].append(cnt*graph_num)
-          # print(f"({cnt}x{graph_num} nodes of {n}x{graph_num} is labeled) Accuracy is guaranteed to be above {threshold*100}%")
-          break
-        elif cnt == n-1:
-          guarantee_ratio_list[n].append(1.0)
-          guarantee_number_list[n].append((cnt+1)*graph_num)
-          print(f"unexpected: cannot guarantee accuracy for {n}x{graph_num} nodes")
 
-    print(f"roop {r+1}/{roop} finished")
+        if 1 - err / len(pred) >= threshold:
+          guarantee_ratio_list[n2].append((cnt*large_graph_num+((cnt//(n1//n2))*(graph_num-large_graph_num)))/(n1*large_graph_num+n2*(graph_num-large_graph_num)))
+          guarantee_number_list[n2].append(cnt*large_graph_num+((cnt//(n1//n2))*(graph_num-large_graph_num)))
+          print(f'Guarantee {threshold} accuracy with {cnt*large_graph_num+((cnt//(n1//n2))*(graph_num-large_graph_num))} labeled nodes in {n1*large_graph_num+n2*(graph_num-large_graph_num)} nodes')
+          break
+        elif cnt == n1-1:
+          guarantee_ratio_list[n2].append(1.0)
+          guarantee_number_list[n2].append(n1*large_graph_num+n2*(graph_num-large_graph_num))
+          print(f"unexpected: cannot guarantee accuracy {threshold*100}%")
+
+    print(f'========== roop {r+1}/{roop} finished ==========')
 
   fig = plt.figure()
-  fig.suptitle(f'The Ratio of Labeled Nodes to Guarantee Accuracy {threshold*100}%\n(calculate mean of {roop} samples)')
+  fig.suptitle(f'The ratio of labeled nodes to guarantee {threshold*100}% accuracy\n(calculate mean of {roop} samples)')
   ax = fig.add_subplot(111)
   ax.plot(guarantee_ratio_list.keys(), [sum(v)/len(v) for v in guarantee_ratio_list.values()])
-  ax.set_xlabel('Number of Nodes per Class')
-  ax.set_ylabel('Ratio of Labeled Nodes')
+  ax.set_xlabel(f'Number of nodes per small class (large class has {n1//n2}times nodes)')
+  ax.set_ylabel('Ratio of labeled nodes')
   ax.grid(axis='y', color='gray', linestyle='--')
-  fig.savefig('./scalefree_graph/task3_figures/task3_mean_a.png')
+  fig.savefig(f'./scalefree_graph/task3_figures/task3_mean_{n1//n2}times_a.png')
 
   fig2 = plt.figure()
-  fig2.suptitle(f'The Number of Labeled Nodes to Guarantee Accuracy {threshold*100}%\n(calculate mean of {roop} samples)')
+  fig2.suptitle(f'The number of labeled nodes to guarantee {threshold*100}% accuracy\n(calculate mean of {roop} samples)')
   ax2 = fig2.add_subplot(111)
   ax2.plot(guarantee_number_list.keys(), [sum(v)/len(v) for v in guarantee_number_list.values()])
-  ax2.set_xlabel('Number of nodes per class')
+  ax2.set_xlabel(f'Number of nodes per small class (large class has {n1//n2}times nodes)')
   ax2.set_ylabel('Number of labeled nodes')
   ax2.grid(axis='y', color='gray', linestyle='--')
-  fig2.savefig('./scalefree_graph/task3_figures/task3_mean_b.png')
+  fig2.savefig(f'./scalefree_graph/task3_figures/task3_mean_{n1//n2}times_b.png')
 
 main()
--- scalefree_graph/task3_2_a.py
+++ scalefree_graph/task3_2_a.py
@@ -1,7 +1,7 @@
 from torch_geometric.nn import GCNConv
 from torch_geometric.utils.convert import from_networkx
 import torch.nn.functional as F
-import sample
+import sample3
 import torch
 import matplotlib.pyplot as plt
 
@@ -30,23 +30,24 @@ class Net(torch.nn.Module):
     return x, y
 
 def main():
-  max_nodes_per_class = 250
-  min_nodes_per_class = 40
+  n = 100
+  m = 2
+  graph_num = 4
+  max_link_level = 96
+  min_link_level = 3
   roop = 20
 
   guarantee_ratio_list = {}
   guarantee_number_list = {}
-  for i in range(min_nodes_per_class, max_nodes_per_class+1, 5):
+  for i in range(min_link_level, max_link_level+1, 3):
     guarantee_ratio_list[i] = []
     guarantee_number_list[i] = []
 
   threshold = 0.8
 
   for r in range(roop):
-    for n in range(min_nodes_per_class, max_nodes_per_class+1, 5):
-      m = 2
-      graph_num = 4
-      a,_,c = sample.generate_sample(n, m, graph_num)
+    for link_level in range(min_link_level, max_link_level+1, 3):
+      a,_,c = sample3.generate_flexible_linked_sample(n, m, graph_num, link_level)
       dataA = from_networkx(a)
 
       device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -58,54 +59,54 @@ def main():
 
         epoch_num = 300
         t = dataA.label
-
         samples = []
         for i in range(cnt):
           for j in c[i]:
             samples.append(j)
 
-        for _ in range(epoch_num):
+        for epoch in range(epoch_num):
           optimizer.zero_grad()
-          _, out = model(dataA)
-          loss = F.nll_loss(out[samples], dataA.label[samples])
+          _, y = model(dataA)
+          loss = F.nll_loss(y[samples], t[samples])
           loss.backward()
           optimizer.step()
+          # print(f'Epoch: {epoch+1:03d}, Loss: {loss:.4f}')
 
         model.eval()
-        _, out = model(dataA)
-        pred = out.max(dim=1)[1]
+        _, y = model(dataA)
+        pred = y.max(dim=1)[1]
         err = 0
-        for i, p in enumerate(pred):
+        for i,p in enumerate(pred):
           if p != t[i]:
             err += 1
-        if 1 - err / len(pred) >= threshold:
-          guarantee_ratio_list[n].append((cnt)/n)
-          guarantee_number_list[n].append(cnt*graph_num)
-          # print(f"({cnt}x{graph_num} nodes of {n}x{graph_num} is labeled) Accuracy is guaranteed to be above {threshold*100}%")
+        if 1 - err/len(pred) >= threshold:
+          guarantee_ratio_list[link_level].append(cnt/n)
+          guarantee_number_list[link_level].append(cnt*graph_num)
+          print(f"({cnt}x{graph_num} nodes of {n}x{graph_num} is labeled) Accuracy is guaranteed to be above {threshold*100}%")
           break
         elif cnt == n-1:
-          guarantee_ratio_list[n].append(1.0)
-          guarantee_number_list[n].append((cnt+1)*graph_num)
+          guarantee_ratio_list[link_level].append(1)
+          guarantee_number_list[link_level].append(n*graph_num)
           print(f"unexpected: cannot guarantee accuracy for {n}x{graph_num} nodes")
 
-    print(f"roop {r+1}/{roop} finished")
+    print(f"=============== roop {r+1}/{roop} is finished ===============")
 
   fig = plt.figure()
-  fig.suptitle(f'The Ratio of Labeled Nodes to Guarantee Accuracy {threshold*100}%\n(calculate mean of {roop} samples)')
+  fig.suptitle(f'The Ratio of Labeled Nodes to Guarantee Accuracy {threshold*100}% in n={n}\n(calculate mean of {roop} samples)')
   ax = fig.add_subplot(111)
   ax.plot(guarantee_ratio_list.keys(), [sum(v)/len(v) for v in guarantee_ratio_list.values()])
-  ax.set_xlabel('Number of Nodes per Class')
+  ax.set_xlabel('Number of Probabilistic Link per 2 Classes')
   ax.set_ylabel('Ratio of Labeled Nodes')
   ax.grid(axis='y', color='gray', linestyle='--')
-  fig.savefig('./scalefree_graph/task3_figures/task3_mean_a.png')
+  fig.savefig(f'./scalefree_graph/task3_figures/task3_link_mean_n{n}_a.png')
 
   fig2 = plt.figure()
-  fig2.suptitle(f'The Number of Labeled Nodes to Guarantee Accuracy {threshold*100}%\n(calculate mean of {roop} samples)')
+  fig2.suptitle(f'The Number of Labeled Nodes to Guarantee Accuracy {threshold*100}% in n={n}\n(calculate mean of {roop} samples)')
   ax2 = fig2.add_subplot(111)
   ax2.plot(guarantee_number_list.keys(), [sum(v)/len(v) for v in guarantee_number_list.values()])
-  ax2.set_xlabel('Number of nodes per class')
+  ax2.set_xlabel('Number of Probabilistic Link per 2 Classes')
   ax2.set_ylabel('Number of labeled nodes')
   ax2.grid(axis='y', color='gray', linestyle='--')
-  fig2.savefig('./scalefree_graph/task3_figures/task3_mean_b.png')
+  fig2.savefig(f'./scalefree_graph/task3_figures/task3_link_mean_n{n}_b.png')
 
 main()
